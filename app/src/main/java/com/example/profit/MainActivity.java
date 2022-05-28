a4e7fcb3b62ff3560f288f87188037924aa015a0
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-05-29 02:58:09 +0530

    Added Splash and login

--- app/src/main/java/com/example/profit/MainActivity.java
+++ app/src/main/java/com/example/profit/MainActivity.java
@@ -4,23 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn = findViewById(R.id.butn);
+    public static int splash_screen = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
+            public void run() {
                 Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        },splash_screen);
     }
 }
